468441b3cd65270775bc1d43cbf854a5ee0e5ac2
cortex05 <havinginitiative@gmail.com>
2025-06-29 15:21:31 -0500

    feat: basic grade level spacing

--- frontend/src/screens/gradeLevel/GradeLevelScreen.tsx
+++ frontend/src/screens/gradeLevel/GradeLevelScreen.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styles from './GradeLevelScreen.module.css'
+import GradeLink from './GradeLink/GradeLink'
 
 const GradeLevelPage = () => {
   return (
-    <div>Grade Level Page</div>
+    <div className='p-8'>
+      <h1 className='text-4xl text-center mb-8'>Click a grade to get an overview</h1>
+      <div className='flex flex-wrap'>
+        <GradeLink />
+        <GradeLink />
+        <GradeLink />
+        <GradeLink />
+        <GradeLink />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/screens/gradeLevel/GradeLink/GradeLink.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './GradeLink.module.css'
+
+const GradeLink = () => {
+  return (
+    <div className={`${styles.link}`}>
+      <h4 className='text-xl'>Holder</h4>
+    </div>
+  )
+}
+
+export default GradeLink
