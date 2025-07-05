beb639d74f259643949b72cc016c02005f9d1651
cortex05 <havinginitiative@gmail.com>
2025-07-05 15:18:21 -0500

    feat: Routing from grade Level to course

--- frontend/src/components/utilities/CourseLink/CourseLink.tsx
+++ frontend/src/components/utilities/CourseLink/CourseLink.tsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from 'react'
 import type { ICourseLink } from '../../../types/types'
 import { handleCourseLinkColor } from '../../../utility/Functions'
 import styles from './CourseLink.module.css'
+import { useNavigate } from 'react-router-dom'
 
 type Props = {
   link: ICourseLink
 }
 const CourseLink = (props: Props) => {
   const { link } = props
+  const navigate = useNavigate();
+  
   const [backColor, setBackColor] = useState<string>("blue")
 
   useEffect(() => {
@@ -15,7 +18,9 @@ const CourseLink = (props: Props) => {
   }, [])
 
   return (
-    <div className={`${backColor} p-1 mr-4 mb-[4px] text-center ${styles.break}`}>
+    <div 
+      className={`${backColor} p-1 mr-4 mb-[4px] text-center ${styles.break}`}
+      onClick={() => navigate(`/course/${link.id}`)}>
       <p>{link.name}</p>
     </div>
   )
