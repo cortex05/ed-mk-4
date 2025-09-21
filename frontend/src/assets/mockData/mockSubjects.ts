0dcac7939186dcf5d9f71001302ba66076b337c7
cortex05 <havinginitiative@gmail.com>
2025-09-21 16:50:42 -0500

    feat: Made basic subject link model and page

---
+++ frontend/src/assets/mockData/mockSubjects.ts
@@ -0,0 +1,23 @@
+export const subjectsWithData = [
+  {
+    id: "MATH",
+    nameDisplay: "Mathematics",
+    courses: [
+      {
+        id: "ALGEBRA_ONE",
+        name: "Algebra 1",
+        gradeLevels: [ "SEVENTH_GRADE"],
+      }
+    ]
+  }, {
+    id: "ENGLISH",
+    nameDisplay: "English",
+    courses: [
+      {
+        id: "NINTH_GRADE_ENGLISH",
+        name: "Ninth Grade English",
+        gradeLevels: [ "NINTH_GRADE"],
+      }
+    ]
+  }
+]
--- frontend/src/screens/subjects/SubjectsScreen.tsx
+++ frontend/src/screens/subjects/SubjectsScreen.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./SubjectScreen.module.css";
+import { subjectsWithData } from "../../assets/mockData/mockSubjects";
 
 const SubjectsPage = () => {
   return (
-    <div>Subjects Page</div>
-  )
-}
+    <div className="p-8">
+      <h1 className="text-4xl text-center mb-8">
+        Click a subject to see what kind of courses are available
+      </h1>
+      <div className="flex flex-wrap justify-center">
+        {subjectsWithData.map((subject) => {
+          return (
+            <div
+              className={`${styles.link}`}
+              onClick={() => console.log("clicked")}
+            >
+              <h4 className="text-xl">{subject.nameDisplay}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default SubjectsPage
+export default SubjectsPage;
